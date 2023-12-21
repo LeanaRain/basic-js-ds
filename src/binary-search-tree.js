49edfd3a759e782f95d38fed883960a936d407e5
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-12-21 21:01:17 +0500

    init: start task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,53 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
-
+const { Node } = require('../extensions/list-tree.js');
+  class Node {
+    tree(data) {
+      // устанавливаем нач. знач-ия св-в конструктора tree
+      this.data = data; // св-во дата для хранения данных
+      this.left = null; // по умолч. узел не имеет лев. потомка
+      this.right = null; // по умолч. узел не имеет прав. потомка
+    }
+  }
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
 
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // добавление данных
+  add(data) {
+    
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // проверка наличия данных
+  has(data) {
+    
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // поиск данных
+  find(data) {
+    
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // удаление данных
+  remove(data) {
+    
   }
 
+  // минимальное значение в дереве
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
+  // максимальное значение в дереве
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
