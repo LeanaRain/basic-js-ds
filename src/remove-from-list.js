36fa54a15ae8a8c4aefc0575f1a5dc3c793d9efa
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-12-22 04:28:18 +0500

    feat: complete remove k from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  if (l === null) {
+    // если эл-т списка = 0, т.е. достигнут конец списка
+    return l;
+    // конец рекурсии и окончание обработки
+  }
+
+  l.next = removeKFromList(l.next, k);
+  // вызыв. ф-ию для след. эл-та списка
+
+  if (l.value === k) {
+    // если значение эл-та к
+    return l.next;
+    // то он удаляется и возв-ся след. эл-т
+  } else {
+    // если эл-т не к
+    return l;
+    // возвр-ся эл-т
+  }
 }
 
 module.exports = {
